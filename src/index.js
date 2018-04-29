f2ecf16075ac2ad2f5890d672da0737329d58501
Anthony <zeitlin1@gmail.com>
2018-04-29 11:11:30 -0400

    fixed type 2

--- src/index.js
+++ src/index.js
@@ -6,12 +6,16 @@ import registerServiceWorker from './registerServiceWorker';
 
 const title = React.createElement('h1', {}, 'My First React Code');
 
+const pp = React.createElement(
+  'p',
+  {},
+  'This react app is a test designed to teach me the fundamentals of how to add and removed elements within the virtual DOM'
+);
+
 const list = React.createElement('div', {}, [
   title,
-  React.createElement('p', {}, [
-    'This react app is a test designed to teach me the fundamentals of how to add and removed elements within the virtual DOM',
-    React.createElement('h2', {}, 'My Favorite Flavors')
-  ]),
+  pp,
+  React.createElement('h2', {}, 'My Favorite Flavors'),
   React.createElement('ul', {}, [
     React.createElement('li', { className: 'gray' }, 'Vanilla'),
     React.createElement('li', { className: 'brown' }, 'Chocolate'),
