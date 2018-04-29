9d5066d7739f213cf91944a2d35633f7d897bb0f
Anthony <zeitlin1@gmail.com>
2018-04-29 11:06:52 -0400

    init commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+const title = React.createElement('h1', {}, 'My first React Code');
+
+const list = React.createElement('div', {}, [
+  title,
+  React.createElement(
+    'p',
+    {},
+    'This react app is a test designed to teach me the fundamentals of how to add and removed elements within the virtual DOM',
+    'My Favorite Flavors'
+  ),
+  React.createElement('ul', {}, [
+    React.createElement('li', { className: 'gray' }, 'Vanilla'),
+    React.createElement('li', { className: 'brown' }, 'Chocolate'),
+    React.createElement('li', { className: 'red' }, 'Strawberry')
+  ])
+]);
+const container = React.createElement('div', {}, list);
+
+ReactDOM.render(container, document.getElementById('root'));
+
+registerServiceWorker();
