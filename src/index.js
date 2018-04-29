531559d626ee3c83626c7c1adb2369e2b5ebd8e1
Anthony <zeitlin1@gmail.com>
2018-04-29 11:09:12 -0400

    fixed type

--- src/index.js
+++ src/index.js
@@ -4,16 +4,14 @@ import './index.css';
 // import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const title = React.createElement('h1', {}, 'My first React Code');
+const title = React.createElement('h1', {}, 'My First React Code');
 
 const list = React.createElement('div', {}, [
   title,
-  React.createElement(
-    'p',
-    {},
+  React.createElement('p', {}, [
     'This react app is a test designed to teach me the fundamentals of how to add and removed elements within the virtual DOM',
-    'My Favorite Flavors'
-  ),
+    React.createElement('h2', {}, 'My Favorite Flavors')
+  ]),
   React.createElement('ul', {}, [
     React.createElement('li', { className: 'gray' }, 'Vanilla'),
     React.createElement('li', { className: 'brown' }, 'Chocolate'),
